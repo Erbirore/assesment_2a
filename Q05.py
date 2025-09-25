4211fa8c33b0045e752508ca3a04acef7c83195a
20155033 <20155033@tafe.wa.edu.au>
2025-09-25 17:25:59 +0800

    original file

---
+++ Q05.py
@@ -0,0 +1,11 @@
+course_units=["ICTSAS432", "ICTPRG302", "ICTDAT401"]
+
+units_count= len(course_units)
+
+print(units_count)
+
+african_animals=["zebra", "elephant", "lion", "rhino", "hippo"]
+
+african_animals.append("cheetah")
+
+print(african_animals)
