7c912c98dfd4bacbef889e934fb7f22f268be611
20155033 <20155033@tafe.wa.edu.au>
2025-09-25 17:40:02 +0800

    implement changes

--- Q05.py
+++ Q05.py
@@ -4,8 +4,8 @@ units_count= len(course_units)
 
 print(units_count)
 
-african_animals=["zebra", "elephant", "lion", "rhino", "hippo"]
+african_animals=["zebra", "elephant", "lion","rhino", "cat"]
 
-african_animals.append("cheetah")
+african_animals.append("snake")
 
 print(african_animals)
